94606c271120ff7b5e158ebf61b98e8b44a43092
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2025-03-06 17:13:33 -0300

    add lib Lottie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+    //Lottie Animations
+    implementation(libs.dotlottie.android)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
