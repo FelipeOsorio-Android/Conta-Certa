57297e99913ca061091fc32e8e2355e0592db9d2
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2025-03-23 15:54:04 -0300

    adjust background color

--- app/src/main/java/com/example/contacerta/ui/screens/CustomSplashScreen.kt
+++ app/src/main/java/com/example/contacerta/ui/screens/CustomSplashScreen.kt
@@ -1,10 +1,13 @@
 package com.example.contacerta.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.lottiefiles.dotlottie.core.compose.runtime.DotLottieController
 import com.lottiefiles.dotlottie.core.compose.ui.DotLottieAnimation
@@ -30,12 +33,18 @@ fun CustomSplashScreen(
         dotController.addEventListener(dotEventListener)
     }
 
-    DotLottieAnimation(
-        source = DotLottieSource.Asset("splash_animated.lottie"),
-        controller = dotController,
-        modifier = modifier.fillMaxSize(),
-        autoplay = true
-    )
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        DotLottieAnimation(
+            source = DotLottieSource.Asset("splash_animated.lottie"),
+            controller = dotController,
+            modifier = modifier.fillMaxSize(),
+            autoplay = true
+        )
+    }
 }
 
 @Preview
